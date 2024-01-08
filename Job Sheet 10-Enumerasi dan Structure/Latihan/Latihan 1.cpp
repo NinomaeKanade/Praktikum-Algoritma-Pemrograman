d01439b07292fdafd774a7d44283d3097f410a5c
NinomaeKanade <155796634+NinomaeKanade@users.noreply.github.com>
2024-01-08 11:31:19 +0700

    Add files via upload

---
+++ Job Sheet 10-Enumerasi dan Structure/Latihan/Latihan 1.cpp
@@ -0,0 +1,16 @@
+/*
+Nama  : Rafli Arianto
+NIM   : 23343051
+Prodi : Informatika
+*/
+
+#include <stdio.h>
+
+enum hari {SENIN, SELASA, RABU, KAMIS, JUMAT, SABTU, MINGGU};
+
+int main(){
+	enum hari sekarang;
+	sekarang = RABU;
+	printf("Sekarang hari ke-%d", sekarang +1);
+	
+}
---
+++ Job Sheet 10-Enumerasi dan Structure/Tugas/Tugas 2 - Menentukan Zodiak.cpp
@@ -0,0 +1,56 @@
+/*
+Nama  : Rafli Arianto
+NIM   : 23343051
+Prodi : Informatika
+*/
+
+#include <stdio.h>
+
+struct Tanggal {
+    int tgl;
+    int bln;
+    int tahun;
+};
+
+struct Zodiak {
+    struct Tanggal awal;
+    struct Tanggal akhir;
+    const char* nama;
+};
+
+struct Zodiak zodiak[12] = {
+    {{20, 1, 0}, {18, 2, 0}, "CAPRICORN"},
+    {{19, 2, 0}, {20, 3, 0}, "PISCES"},
+    {{21, 3, 0}, {19, 4, 0}, "ARIES"},
+    {{20, 4, 0}, {20, 5, 0}, "TAURUS"},
+    {{21, 5, 0}, {20, 6, 0}, "GEMINI"},
+    {{21, 6, 0}, {22, 7, 0}, "CANCER"},
+    {{23, 7, 0}, {22, 8, 0}, "LEO"},
+    {{23, 8, 0}, {22, 9, 0}, "VIRGO"},
+    {{23, 9, 0}, {22, 10, 0}, "LIBRA"},
+    {{23, 10, 0}, {21, 11, 0}, "SCORPIO"},
+    {{22, 11, 0}, {21, 12, 0}, "SAGITTARIUS"},
+    {{22, 12, 0}, {19, 1, 0}, "CAPRICORN"}
+};
+
+int main() {
+    struct Tanggal tanggal_lahir;
+
+    printf("Tanggal Lahir Anda [tgl-bln-tahun] : ");
+    scanf("%d-%d-%d", &tanggal_lahir.tgl, &tanggal_lahir.bln, &tanggal_lahir.tahun);
+
+    int i;
+    for (i = 0; i < 12; i++) {
+        if ((tanggal_lahir.bln == zodiak[i].awal.bln && tanggal_lahir.tgl >= zodiak[i].awal.tgl) ||
+            (tanggal_lahir.bln == zodiak[i].akhir.bln && tanggal_lahir.tgl <= zodiak[i].akhir.tgl)) {
+            printf("Zodiak Anda adalah : %s\n", zodiak[i].nama);
+            break;
+        }
+    }
+
+    if (i == 12) {
+        printf("Tanggal lahir tidak valid.\n");
+    }
+
+    return 0;
+}
